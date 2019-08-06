3cf67407808e8b75851f7ede69ceebfca821257b
Aramis <emnasirianfar@gmail.com>
2019-08-06 07:20:06 +0430

    for learning

--- src/OopCar.java
+++ src/OopCar.java
@@ -2,7 +2,12 @@ public class OopCar {
     private int color;
     private String model;
     private boolean transmition; // true for automatic
+    // Constructor with default values
+    public OopCar(){
+        this.model = "Default value is now working "
+                + "because you havent set anything.";
 
+    }
 
     public void setModel(String model) {
         if (model.equals("BMW-X6") || model.equals("Mustang-GT500")) {
--- src/OopNewCarDispatch.java
+++ src/OopNewCarDispatch.java
@@ -4,6 +4,7 @@ public class OopNewCarDispatch {
         OopCar bmw = new OopCar();
         OopCar mustang = new OopCar();
         OopCar mercedes = new OopCar();
+        OopCar test777 = new OopCar();
 
         String model = "BMW-X6";
         String model2 = "Mercedes-SLS";
@@ -17,9 +18,13 @@ public class OopNewCarDispatch {
         mercedes.setModel(model2);
         mustang.setModel(model3);
 
+        //giving no value to trigger the constructor to work
+        System.out.println(test777.getModel()); //Outputs BMW-X6
+
+       //printing out what we've sent to the class
        System.out.println(bmw.getModel()); //Outputs BMW-X6
-       System.out.println(mercedes.getModel()); //Outputs BMW-X6
-       System.out.println(mustang.getModel()); //Outputs BMW-X6
+       System.out.println(mercedes.getModel()); //Outputs Mercedes-SLS which is not as hot as other cars (kidding)
+       System.out.println(mustang.getModel()); //Outputs Mustang-GT500
 
     }
 }
